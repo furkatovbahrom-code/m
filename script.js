aa5926bb5bc7342e19750aebafdca6fcc8a3fcd1
furkatovbahrom-code <furkatovbahrom@gmail.com>
2026-02-10 00:57:51 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("toggle");
+const radial = document.getElementById("radial");
+
+toggle.addEventListener("click", () => {
+  radial.classList.toggle("active");
+});
